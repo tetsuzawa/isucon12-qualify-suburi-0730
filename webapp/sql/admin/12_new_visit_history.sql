f2df35468228cc1f20089efdf0ca8ad3f1110e17
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2022-08-21 20:55:45 +0900

    Revert "無駄なvisit_historyを削除する"

--- webapp/sql/admin/12_new_visit_history.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE visit_history_2 AS
-SELECT tenant_id, player_id, competition_id, MIN(created_at) AS created_at FROM visit_history GROUP BY tenant_id, player_id, competition_id;
-ALTER TABLE visit_history RENAME TO visit_history_old;
-ALTER TABLE visit_history_2 RENAME TO visit_history;
-CREATE UNIQUE INDEX visit_history_idx ON visit_history(tenant_id, player_id, competition_id);
