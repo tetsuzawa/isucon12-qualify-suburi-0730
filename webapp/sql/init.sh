e02369b2a9345805d9dd109df51afcb9a2d494d2
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2022-08-29 16:24:42 +0900

    init tennant admin

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -16,6 +16,13 @@ mysql -u"$ISUCON_DB_USER" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < init.sql
 
+# MySQLを初期化
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" < admin/dump_13_tables.sql
+
 # SQLiteのデータベースを初期化
-rm -f ../tenant_db/*.db
-cp -r ../../initial_data/*.db ../tenant_db/
+#rm -f ../tenant_db/*.db
+#cp -r ../../initial_data/*.db ../tenant_db/
